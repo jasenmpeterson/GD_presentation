ca3f78273c4834ae8bf609b198d5d8414bf6008e
Jasen Peterson <misc@jasenpeterson.com>
2017-10-16 15:54:12 -0500

    first commit

---
+++ page-templates/home.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: Home
+*/
+
+// page id
+
+$page_id = get_queried_object_id();
+
+// assets
+$hero_image_id = get_post_thumbnail_id( $page_id );
+
+get_header();
+
+?>
+
+	<section class="gd-homepage-hero" style="background: url('<?php echo wp_get_attachment_url($hero_image_id);?>')">
+		<div class="grid-x align-middle">
+
+			<div class="cell medium-5 drilling">
+				<div class="section-button">
+					Drilling
+				</div>
+			</div>
+
+			<div class="cell medium-2 svg-wrap">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/svg/gd-logo.svg" alt="">
+			</div>
+
+			<div class="cell medium-5 fracking">
+				<div class="section-button">
+					Fracking
+				</div>
+			</div>
+
+		</div>
+	</section>
+
+<?php get_footer();
